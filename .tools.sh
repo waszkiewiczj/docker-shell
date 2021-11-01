4b4aee05a3def028bec202322ca663068d6279a1
Jakub <waszkiewiczj.dev@outlook.com>
2021-11-01 15:26:00 +0100

    move vim to additional tools

--- .tools.sh
+++ .tools.sh
@@ -14,3 +14,15 @@ alias iftop='docker run \
 --interactive \
 --net host \
 nixery.dev/iftop iftop'
+
+
+# vim
+alias vim='docker run \
+--rm \
+--tty \
+--interactive \
+--env VIMINIT \
+--volume /config/.vimrc:/config/.vimrc \
+--volume ${HOME}:${HOME} \
+nixery.dev/vim/vimplugins.onedark-vim \
+vim --cmd "set runtimepath+=/"'
