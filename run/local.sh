027bcca6b49ab6f6805737028d2460ca049d4ec2
Jakub Waszkiewicz <waszkiewicz.jm+github@protonmail.com>
2021-11-02 23:56:08 +0000

    move run script

---
+++ run/local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOCAL_SCRIPT_LOCATION="$(dirname $0)"
+DOCKER_SHELL_IMAGE_TAG="docker-shell"
+DOCKER_BUILDKIT=1
+
+docker build --progress plain --tag "${DOCKER_SHELL_IMAGE_TAG}" .
+
+if [[ -f "${LOCAL_SCRIPT_LOCATION}/template.sh" ]]; then
+	source "${LOCAL_SCRIPT_LOCATION}/template.sh"
+else
+	echo "ERROR! No template script available!"
+fi
+
